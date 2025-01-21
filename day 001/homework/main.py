aaa7294ff96227408712cd53de8fea90c01644c3
ilia543 <mrmainxpx@gmail.com>
2025-01-21 21:45:03 +0400

    algoritmebi gak 1 :)

---
+++ day 001/homework/main.py
@@ -0,0 +1,80 @@
+# დავალება ნომერი 1
+# def func1(a ,b ,c):
+#     if b > a:
+#         a, b = b, a
+#         return (a - b - 2) // c
+#     else:
+#         return(a - b - 2) // c
+
+# print(func1(20, 40, 2))
+
+
+#დავალება ნომერი 2
+# def func2():
+#     number = 75
+#     numb_Binary = []
+
+#     while(number > 0):
+
+#         Binary_0_1_numbers = number % 2
+
+#         numb_Binary.append(str(Binary_0_1_numbers))
+
+#         number = number // 2
+
+#     return "".join(numb_Binary[::-1])
+
+# print(func2())
+
+
+
+
+
+
+
+
+
+#დავალება ნომერი 3
+# a = 5
+# b = 14
+# c = 132
+# d = 144
+# def func3():
+#     if a > b:
+#         if a > c:
+#             if a > d:
+#                 return a
+#             else:
+#                 return d
+#         else:
+#             if c > d:
+#                 return c
+#             else:
+#                 return d
+#     else:
+#         if b > c:
+#             if b > d:
+#                 return b
+#             else:
+#                 return d
+#         else:
+#             if c > d:
+#                 return c
+#             else:
+#                 return d
+
+
+# def func3_1():
+
+#     largest = a
+
+#     if b > largest:
+#         largest = b
+        
+#     if c > largest:
+#         largest = c
+
+#     if d > largest:
+#         largest = d
+
+#     return largest
