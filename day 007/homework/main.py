886aa0907503e830c8a46eaa9c9952d07cefd272
ilia543 <mrmainxpx@gmail.com>
2025-02-21 21:04:04 +0400

    second commit

--- day 007/homework/main.py
+++ day 007/homework/main.py
@@ -1,25 +1,52 @@
 def func(numb, whicsystem):
-    if whicsystem == "decimal to binary":
+    if whicsystem == "decimal to binary" or whicsystem == "decimal to octal" or whicsystem == "decimal to hexadecimal":
+
         numb = int(numb)
         result = []
+
         while numb != 0:
-            result.append(str(numb % 2))
-            numb //= 2
+            if whicsystem == "decimal to binary":
+                result.append(str(numb % 2))
+                numb //= 2
+
+            elif whicsystem == "decimal to octal":
+                result.append(str(numb % 8))
+                numb //= 8
+
+            elif whicsystem == "decimal to hexadecimal":
+                result.append(str(numb % 16))
+                numb //= 16
+
         result = result[::-1]
         return "".join(result)
+    
     else:
+
         xarisxi = -1
+
         numb = numb.split()
         numb = numb[::-1]
+
         result = []
+
         for i in numb:
+
             xarisxi += 1
+
             if whicsystem == "binary to decimal":
                 result.append(int(i) * (2 ** xarisxi))
+
             elif whicsystem == "octal to decimal":
-                result.append(int(i) * (8 ** xarisxi))
+
+                if int(i) >= 8:
+                    return "ესეთი რიცხვი რვაობით სისტემაში არ არსებობს"
+                
+                else:
+                    result.append(int(i) * (8 ** xarisxi))
+
             elif whicsystem == "hexadecimal to decimal":
                 result.append(int(i) * (16 ** xarisxi))
+
         return sum(result)
     
     
@@ -27,3 +54,5 @@ print(func("1 1 1 1 0 1 0 0 1", "binary to decimal"))
 print(func("1 3 4 6", "octal to decimal"))
 print(func("1 3 4 6", "hexadecimal to decimal"))
 print(func("53", "decimal to binary"))
+print(func("127", "decimal to octal"))
+print(func("55", "decimal to hexadecimal"))
